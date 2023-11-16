7efdff1a07ddc4388b70016793c863d52fc8c26e
Ameer <ameerapex496@gmail.com>
2023-11-16 13:46:37 +0100

    added count down page dynamcily + styling

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
-console.log("hallo world");
+// selecting our elements
+const gameWrap = document.getElementsByClassName("game-Wrap")[0];
+
+// our objects
+
+// our functions
+const startGameHandler = (e) => {
+  var dataValue = e.target.getAttribute("data-value");
+
+  // Logging the value to the console
+  console.log(dataValue);
+
+  gameWrap.innerHTML = "";
+
+  let countDownNum = document.createElement("p");
+  countDownNum.classList = "count-Down-Num";
+
+  gameWrap.appendChild(countDownNum);
+  countDownNum.textContent = "3";
+  setTimeout(() => {
+    countDownNum.textContent = "2";
+  }, 1000);
+  setTimeout(() => {
+    countDownNum.textContent = "1";
+  }, 2000);
+  setTimeout(() => {
+    countDownNum.textContent = "GO!";
+  }, 3000);
+  //   setTimeout(() => {
+  //     gameWrap.innerHTML = "";
+  //     startGameHandler(e);
+  //   }, 4000);
+};
+
+// our eventlistners
+gameWrap.addEventListener("click", startGameHandler);
